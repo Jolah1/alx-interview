0cf3fe3feb7855977b032ecc5f0bd0a0042b3a83
okunlade Jolade <jokunlade@gmail.com>
2023-01-25 01:18:00 +0100

    pascal triangle created

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''
+Module of Pascal's triangle of n
+'''
+
+def pascal_triangle(n):
+    '''
+    Returns a list of lists of integers representing the Pascal's triangle of n
+    '''
+
+    if n <= 0:  # if n is not a positive integer
+        return []
+    triangle = [[1]]  # initialize the first row of the triangle
+    for i in range(1, n):  # for each row in the triangle
+        row = [1]  # initialize the first element of the row
+        for j in range(1, i):  # for each element in the row
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+            # add the two elements to the row
+        row.append(1)  # add the last element to the row
+        triangle.append(row)  # add the row to the triangle
+    return triangle  # return the triangle
